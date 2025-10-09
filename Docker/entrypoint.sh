b85457a6d08aded37b991a405e52e5f5d229490d
Devis Dal Moro <devis.dalmoro@eurecat.org>
2025-10-09 09:05:53 +0200

    nvidia flag needed on .env

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -26,8 +26,6 @@ if [ -f "/workspace/install/setup.bash" ]; then
 fi
 
 source /opt/ros_python_env/bin/activate
-echo "Python version: $(python3 --version)"
-echo "Torch version: $(python3 -c 'import torch; print(torch.__version__)')"
 echo "=== ENTRYPOINT END $(date) PID=$$ ==="
 
 # Execute the command passed to the container
